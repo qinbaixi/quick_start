c01dcb7c682c61b89bdbf2c05bd725f5fb831d3f
Bycc <1377519410@qq.com>
2024-12-25 22:22:28 +0800

    修改sh脚本以及README

--- README.md
+++ README.md
@@ -5,5 +5,11 @@ An OTP application
 
 Build
 -----
-run scripts/qcompile.bat
-then run scripts/start_all.bat
+Windows(推荐)
+执行编译脚本 scripts/qcompile.bat
+执行开启脚本 scripts/start_all.bat
+
+修改启动节点配置（如节点名、节点启动数量） 可以在config.bat中修改，
+
+Linux
+将sh目录脚本复制到scripts下， 操作同Windows，后缀文件换成sh
--- scripts/sh/qcompile.sh
+++ scripts/sh/qcompile.sh
@@ -5,13 +5,12 @@ source ./config.sh
 cd ..
 
 erlc -o ebin ./src/tools/qmake.erl
-erlc -I include -o ebin ./src/tools/lager/lager_rotator_behaviour.erl
 
 echo "Start Compiling..."
 echo
 echo
 
-erl -pa ./ebin -eval "case qmake:compile(${compile_concurrency}, []) of ok -> halt(0); _ -> halt(1) end." -noshell -s init stop
+erl -pa ./ebin -eval "case qmake:compile(${compile_concurrency}) of success -> halt(0); _ -> halt(1) end." -noshell -s init stop
 
 compile_res=$?
 
